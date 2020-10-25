b971d45c0f94e279dd0a87bf04061769178733c5
Tin Luu <trungtin.luu@gmail.com>
2020-10-24 18:46:53 -0700

    Like and dislike button

--- like_button.js
+++ like_button.js
@@ -10,14 +10,22 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
-    }
+      return e('button',
+      { onClick: () => this.setState({ liked: false }) },
+      'Dislike')
+    };
 
     return e(
       'button',
       { onClick: () => this.setState({ liked: true }) },
       'Like'
     );
+  }
+}
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
   }
 }
 
